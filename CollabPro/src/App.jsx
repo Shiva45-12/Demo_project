d8231117dc44446b9d7478017a600007bfdc5de5
SHIVA VISHWAKARMA <raw66010@gmail.com>
2025-09-20 14:33:38 +0530

    second change
    second change

--- CollabPro/src/App.jsx
+++ CollabPro/src/App.jsx
@@ -6,6 +6,7 @@ function App() {
   return (
    <div>
 <h1>Collaboration</h1>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat facere minus laboriosam officiis nesciunt, porro nobis impedit dolor saepe deleniti expedita, excepturi nulla! Quisquam eligendi ad at dolorum assumenda rem voluptatum in eveniet iure consequuntur officiis, doloribus ut excepturi numquam veritatis accusamus maiores! Omnis mollitia eos consequatur et quidem cum iste nam nesciunt, totam ut similique rerum deleniti doloremque sequi? Quod, maxime. Consequuntur sed corporis asperiores, voluptatum dolor dolorem ipsam? Nesciunt reprehenderit voluptates debitis magnam incidunt quod possimus in laborum fugiat veritatis tenetur ipsam, nam perspiciatis ea libero recusandae quisquam vero asperiores vel quis unde reiciendis illo iusto harum? Molestiae.</p>
    </div>
   )
 }
